3082109caf3a974f8dde5b43739f2a1be53c0c4a
yoho <yohovanivargas@gmail.com>
2023-06-29 20:29:18 -0600

    useState

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,28 +1,19 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom/client'
+import { useState } from 'react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const users = [
-    {
-        id: 1,
-        name: 'Yoho',
-        image: 'https://robohash.org/user1'
-    },
-    {
-        id: 2,
-        name: 'Joe',
-        image: 'https://robohash.org/user2'
-    }
-]
-root.render(
-    
-    <>
-        {users.map((user,i) => {
-            return <div key={i}>
-                <h1>{user.name}</h1>
-                <img src={user.image}/>
-                </div>
-        })}
+function Counter(){
+
+    var [mensaje, setMensaje] = useState('')
+
+    return<>
+        <input onChange={e => setMensaje(e.target.value)}/>
+        <button onClick={() => alert('El mensaje es: '+mensaje)}>Save</button>
     </>
+
+}
+root.render(
+    <Counter />
 )
