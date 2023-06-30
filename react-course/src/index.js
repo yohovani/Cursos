11f7d1d4e6017f6650b0e6a7d7eea9c3b2e7a2f9
yoho <yohovanivargas@gmail.com>
2023-06-29 20:39:52 -0600

    useEfect

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import  ReactDOM  from 'react-dom/client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 function Counter(){
 
-    var [mensaje, setMensaje] = useState('')
+    var [mensaje, setMensaje] = useState('');
+    var [counter, setCounter] = useState(0)
+
+    useEffect(function () {
+        console.log("Render")
+    }, [counter])
 
     return<>
         <input onChange={e => setMensaje(e.target.value)}/>
         <button onClick={() => alert('El mensaje es: '+mensaje)}>Save</button>
+        <h1>Counter: {counter}</h1>
+        <button onClick={() => setCounter(counter + 1)}>Sumar</button>
+        <button onClick={() => setCounter(counter - 1)}>Restar</button>
+        <button onClick={() => setCounter(counter = 1)}>Reiniciar</button>
     </>
 
 }
